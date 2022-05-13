64d7630347f602f0d969107f043444a8fbf40c09
PekyPirogi <v.k.martynov@yandex.ru>
2022-05-14 00:14:10 +0500

    HW_1 for check

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Вячеслав Мартынов on 13.05.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var imageViewAvatar: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelBirth: UILabel!
+    @IBOutlet weak var labelCity: UILabel!
+    @IBOutlet weak var textViewDescription: UITextView!
+    
+    
+}
