edf786e5347e27c88dfd5d777dbede3288a13828
PekyPirogi <v.k.martynov@yandex.ru>
2022-05-15 20:54:36 +0500

    Feed and Profile tabs added

--- Navigation/Navigation/AppDelegate.swift
+++ Navigation/Navigation/AppDelegate.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 
+let mainTabBarController = MainTabBarController()
+
+
+
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
