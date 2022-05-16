e4bdda89d35cf9c3da9f5eafe2452a77b526100e
PekyPirogi <v.k.martynov@yandex.ru>
2022-05-16 22:12:27 +0500

    ProfileHeaderView ready for edition

---
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Вячеслав Мартынов on 16.05.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileHeaderView: UIView {
+    
+}
+    
+
+    
+
--- Navigation/Navigation/ProfileViewController.swift
+++ Navigation/Navigation/ProfileViewController.swift
@@ -7,23 +7,27 @@
 
 import UIKit
 
+
+
 class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+        
+        viewWillLayoutSubviews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillLayoutSubviews() {
+        let profileHeaderVC = ProfileHeaderView()
+        profileHeaderVC.frame = view.safeAreaLayoutGuide.layoutFrame
+        view.addSubview(profileHeaderVC)
+        
     }
-    */
+    
+   
+        
+   
 
 }
