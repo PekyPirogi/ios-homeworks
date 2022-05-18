8be0bdc2234fdb56413c1c16a863cc0eee8fa542
PekyPirogi <v.k.martynov@yandex.ru>
2022-05-18 12:53:12 +0500

    Final commit, all done

--- Navigation/Navigation/FeedViewController.swift
+++ Navigation/Navigation/FeedViewController.swift
@@ -14,23 +14,63 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        postButton()
+        addPostStackView()
 
         // Do any additional setup after loading the view.
     }
+
+    private let postStackView: UIStackView = {
+       let postSV = UIStackView()
+        postSV.translatesAutoresizingMaskIntoConstraints = false
+        postSV.axis = .vertical
+        postSV.distribution = .fillEqually
+        postSV.spacing = 10
+        
+        return postSV
+    }()
     
-    private func postButton() {
-        let postButton = UIButton(frame: CGRect(x: 200, y: 100, width: 200, height: 50))
+    private func addPostStackView() {
+        view.addSubview(postStackView)
+        
+        [postButton, postButton1].forEach { postStackView.addArrangedSubview($0) }
+        
+        NSLayoutConstraint.activate([
+            postStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            postStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            postButton.widthAnchor.constraint(equalTo: view.widthAnchor)
+           
+        ])
+        
+        NSLayoutConstraint.activate([
+            postButton1.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+    }
+    
+    private let postButton: UIButton = {
+        let postButton = UIButton()
         postButton.setTitle("Post", for: .normal)
         postButton.backgroundColor = .gray
         postButton.addTarget(self, action: #selector(postShow), for: .touchUpInside)
-        view.addSubview(postButton)
-    }
+        
+        return postButton
+    }()
+    
+    private let postButton1: UIButton = {
+        let postButton = UIButton()
+        postButton.setTitle("Post", for: .normal)
+        postButton.backgroundColor = .gray
+        postButton.addTarget(self, action: #selector(postShow), for: .touchUpInside)
+        
+        return postButton
+    }()
     
     @objc private func postShow() {
         let postVC = PostViewController()
         navigationController?.pushViewController(postVC, animated: true)
     }
-                          
+    
                           
 }
--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -22,10 +22,10 @@ class ProfileHeaderView: UIView {
         
         
         
-        [avatarImageView, fullNameLabel, statusLabel, setStatusButton, statusTextField].forEach { addSubview($0) }
+        [avatarImageView, fullNameLabel, statusLabel, setStatusButton, statusTextField, aimlessButton].forEach { addSubview($0) }
         
         NSLayoutConstraint.activate([
-            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            avatarImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
             avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             avatarImageView.widthAnchor.constraint(equalToConstant: 100),
             avatarImageView.heightAnchor.constraint(equalToConstant: 100)
@@ -53,6 +53,12 @@ class ProfileHeaderView: UIView {
             statusTextField.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
             statusTextField.heightAnchor.constraint(equalToConstant: 40),
             statusTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            aimlessButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            aimlessButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
+            aimlessButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 0)
         ])
         
         
@@ -129,6 +135,22 @@ class ProfileHeaderView: UIView {
     @objc private func statusTextChanged(_ textField: UITextField) {
         statusText.removeAll()
         statusText.append(textField.text ?? "")
+    }
+    
+    private lazy var aimlessButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("I am aimless", for: .normal)
+        button.layer.cornerRadius = 4
+        button.addTarget(self, action: #selector(doNothing), for: .touchUpInside)
+         
+        return button
+     }()
+    
+    @objc private func doNothing() {
+        
     }
 
 }
